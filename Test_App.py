8b93cf7b51b59875d453e3dacec7d9e7fbbf51ff
Sai-Deepak-1 <psaideepak1@gmail.com>
2023-11-07 23:53:25 +0530

    New Latest Changes made

--- Test_App.py
+++ Test_App.py
@@ -5,27 +5,30 @@ import face_recognition
 import os
 import csv
 
+# Initialize the Flask app
 app = Flask(__name__)
 
-# Specify the directory path where the training images are stored
+# Set the directory path where the training images are stored
 path = 'training-images'
-# Initialize an empty list to store the training images
+
+# Set the directory for serving static files (e.g., CSS, JS, images)
+app.static_folder = 'static'
+
+# Initialize empty lists to store the training images and their corresponding voter IDs
 images = []
-# Initialize an empty list to store the voter IDs
 voterIDs = []
+
 # Get the list of files in the specified directory
 voterList = os.listdir(path)
 
-current_user = None
-
-app.static_folder = 'static'
-
+# Read in the training images and store them in the images list, and store the corresponding voter IDs in the
+# voterIDs list
 for vid in voterList:
     curImg = cv2.imread(f'{path}/{vid}')
     images.append(curImg)
     voterIDs.append(os.path.splitext(vid)[0])
 
-
+# Define a Voter class to store voter information
 class Voter:
     def __init__(self, name):
         self.name = name
@@ -35,16 +38,26 @@ class Voter:
         if self.voted:
             return "Already Voted"
         else:
+            # Open the CSV file for append mode which means both reading and writing
             with open('Voters-List.csv', 'a+', newline='') as f:
+                # Initialize a CSV writer object
                 writer = csv.writer(f)
+
+                # Go to the beginning of the file
                 f.seek(0)
+
+                # Read in the contents of the file and store them in a list
                 voterDataList = [line for line in csv.reader(f)]
+
+                # Extract the names of the voters from the list of voter data
                 voter_names = [row[1] for row in voterDataList]
 
+                # Check if the current voter has already voted
                 if self.name in voter_names:
                     self.voted = True
                     return "Already Voted"
                 else:
+                    # If the current voter has not already voted, write their name and "Voted" to the CSV file
                     writer.writerow([len(voterDataList) + 1, self.name, "Voted"])
                     self.voted = True
                     return "Voted"
@@ -64,57 +77,92 @@ def findFaceEncodings(images):
 
 
 # Get the face encodings for the known voters' images
-
 knownVoterFaceEncodings = findFaceEncodings(images)
+
+# Open a video capture device
 cap = cv2.VideoCapture(0)
 
+# Create an empty list to store voter objects
 voters = []
+
+# Loop through each voter ID and create a Voter object with the uppercase name
 for name in voterIDs:
     voters.append(Voter(name.upper()))
 
-
+# Define a function to generate video frames
 def gen_frames():
+    # Use the global variable current_user
     global current_user
 
+    # Loop indefinitely
     while True:
+        # Read a frame from the video capture device
         success, img = cap.read()
+
+        # Resize the frame to a quarter of its size to improve it's speed
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+
+        # Convert the color space of the frame from BGR to RGB
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
+        # Detect the face locations in the current frame
         facesCurFrame = face_recognition.face_locations(imgS)
+
+        # Encode the faces in the current frame
         faceEncodingsCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
+        # Iterate through each face encoding and location in the current frame
         for faceEncode, faceLoc in zip(faceEncodingsCurFrame, facesCurFrame):
+            # Compare the face encoding to the known voter face encodings
             matches = face_recognition.compare_faces(knownVoterFaceEncodings, faceEncode)
+
+            # Calculate the face distances between the current face encoding and the known voter face encodings
             faceDis = face_recognition.face_distance(knownVoterFaceEncodings, faceEncode)
 
+            # Get the index of the known voter face encoding with the smallest face distance
             matchIndex = np.argmin(faceDis)
 
+            # If the current face encoding matches a known voter face encoding
             if matches[matchIndex]:
+                # Get the corresponding voter object
                 voter = voters[matchIndex]
-                current_user = voter.name  # Set the current recognized user
 
+                # Set the current recognized user to the name of the voter
+                current_user = voter.name
+
+                # Get the face location coordinates in the original size frame
                 y1, x2, y2, x1 = faceLoc
                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                color = (0, 255, 0)
 
+                # Set the color of the rectangle based on whether the voter has voted
+                color = (0, 255, 0)
                 if voter.voted:
                     color = (0, 0, 255)
 
+                # Draw a rectangle around the face in the original size frame
                 cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+                # Draw a filled rectangle below the face in the original size frame
                 cv2.rectangle(img, (x1, y2 - 35), (x2, y2), color, cv2.FILLED)
+
+                # Write the name of the voter below the face in the original size frame
                 cv2.putText(img, voter.name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+                # Mark the voter as voted
                 voter.markAsVoted()
 
-        # processed_frame = process_frame(img)
-        # ret, buffer = cv2.imencode('.jpg', processed_frame)
+        # Encode the processed frame as a JPEG image
         ret, buffer = cv2.imencode('.jpg', img)
+
+        # Convert the image buffer to bytes
         frame = buffer.tobytes()
-        yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'
+
+        # Yield the frame as a multipart MIME message
+        yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r'
+
 
 # Render the index.html template
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
@@ -125,53 +173,14 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# Process the uploaded file
-@app.route('/process', methods=['POST'])
-def process():
-    if 'file' not in request.files:
-        flash('No file uploaded!')
-        return render_template('index.html')
-
-    file = request.files['file']
-
-    if file.filename == '':
-        flash('No file selected!')
-        return render_template('index.html')
-
-    if file:
-        file_path = os.path.join(path, file.filename)
-        file.save(file_path)
-        # Load the uploaded image
-        unknown_image = face_recognition.load_image_file(file_path)
-        # Encode the face in the image
-        unknown_encoding = face_recognition.face_encodings(unknown_image)
-
-        if len(unknown_encoding) > 0:
-            unknown_encoding = unknown_encoding[0]
-
-            for voter in voterList:
-                match = face_recognition.compare_faces([voter['encoding']], unknown_encoding)
-                if match[0]:
-                    flash('Voter Identified: {}'.format(voter['name']))
-                    return render_template('index.html')
-
-            flash('Unknown Voter!')
-            return render_template('index.html')
-
-        else:
-            flash('No face detected in the uploaded image!')
-            return render_template('index.html')
-
-    flash('Error processing the file!')
-    return render_template('index.html')
-
+# Global variable to track authentication
+authenticate = False
 
 # Handle the login page
-authenticate = False  # Global variable to track authentication
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    global authenticate  # Declare authenticate as global
+    # Declare authenticate as global
+    global authenticate
 
     if request.method == 'POST':
         username = request.form['username']
@@ -180,26 +189,32 @@ def login():
         # Check if the login credentials are correct (e.g., admin:admin)
         if username == 'admin' and password == 'admin':
             authenticate = True
-            return redirect('/voted_persons')
+            # Redirect the user to the voted_persons page.
+            return redirect('/voted_persons')  
         else:
-            flash('Invalid username or password', 'danger')
-            return redirect('/login')
-
+             # If the username or password are incorrect, flash a message to the user.
+            flash('Invalid username or password', 'danger') 
+            # Redirect the user back to the login page.
+            return redirect('/login')  
+        
+    # If the request method is not POST, render the login template.
     return render_template('login.html')
-
 
 # Render the list of voted persons
 @app.route('/voted_persons')
 def voted_persons_list():
-    if not authenticate:
+    # Check if the user is authenticated, if not, redirect them to the access_denied page.
+    if not authenticate:  
         return redirect('/access_denied')
-    voted_persons = [voter.name for voter in voters if voter.voted]
-    return render_template('voted_persons.html', voted_persons=voted_persons, current_user=current_user)
+    # Get the names of all voted persons.
+    voted_persons = [voter.name for voter in voters if voter.voted]  
+    # Render the template with the list of voted persons and the current user.
+    return render_template('voted_persons.html', voted_persons=voted_persons, current_user=current_user)  
 
 @app.route('/access_denied')
 def access_denied():
-    return render_template('access_denied.html')
-
+    # Render the access_denied template if the user is not authenticated.
+    return render_template('access_denied.html')  
 
 # Function to draw a square around a face in a frame
 def draw_square(frame, coordinates):
